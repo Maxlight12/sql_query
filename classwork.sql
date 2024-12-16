a83b58f9fd93c31dc2e440c873dc2b84a0265dd9
Maxlight12 <babeela12mira@gmail.com>
2024-12-16 14:20:08 +0100

    Add files via upload

---
+++ classwork.sql
@@ -0,0 +1,68 @@
+-- QUE 1
+SELECT * FROM PRODUCT;
+
+-- QUE 2
+SELECT productname, unitprice from product;
+
+-- que 3
+
+SELECT * from customer
+where customer_table;
+
+ -- que 4
+ 
+ select * from salesorder
+ where orderDate = 1997;
+
+ 
+   -- que 5
+  
+  select * from supplier
+  where country = "USA";
+  
+   -- ques 6
+   
+   select categoryName, description from category;
+   
+   
+     -- que 7
+     
+select firstname,lastname from employee;
+
+  -- que 8
+  
+  select * from product
+  where unitPrice > 50;
+  
+    -- ques 9
+    
+select * from product
+where unitsinstock < 20;
+
+  -- que 10
+  
+  select * from employee 
+  where hiredate > "1995-01-01";
+  
+
+
+  
+  
+  
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+  
+  
+  
+  
+  
+
